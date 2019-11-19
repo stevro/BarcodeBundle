2e14b26ea14e3602fbcc9d303894d2b0bd5db585
Stefan Matei <stefan@nimasoftware.com>
2019-11-19 15:33:45 +0000

    added types

---
+++ Model/Type.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Stev\BarcodeBundle\Model;
+
+/**
+ * Class Type
+ *
+ * Taken from Com\Tecnick\Barcode::$typeclass
+ */
+final class Type
+{
+    const C128a = 'C128A';
+    const C128B = 'C128B';
+    const C128C = 'C128C';
+    const C128 = 'C128';
+    const C39EPlus = 'C39E+';
+    const C39E = 'C39E';
+    const C39Plus = 'C39+';
+    const C39 = 'C39';
+    const C93 = 'C93';
+    const CODABAR = 'CODABAR';
+    const CODE11 = 'CODE11';
+    const EAN13 = 'EAN13';
+    const EAN2 = 'EAN2';
+    const EAN5 = 'EAN5';
+    const EAN8 = 'EAN8';
+    const I25Plus = 'I25+';
+    const I25 = 'I25';
+    const IMB = 'IMB';
+    const IMBPRE = 'IMBPRE';
+    const KIX = 'KIX';
+    const MSIPlus = 'MSI+';
+    const MSI = 'MSI';
+    const PHARMA2T = 'PHARMA2T';
+    const PHARMA = 'PHARMA';
+    const PLANET = 'PLANET';
+    const POSTNET = 'POSTNET';
+    const RMS4CC = 'RMS4CC';
+    const S25Plus = 'S25+';
+    const S25 = 'S25';
+    const UPCA = 'UPCA';
+    const UPCE = 'UPCE';
+    const DATAMATRIX = 'DATAMATRIX';
+    const PDF417 = 'PDF417';
+    const QRCODE = 'QRCODE';
+    const LRAW = 'LRAW';
+    const SRAW = 'SRAW';
+}
--- Service/BarcodeGenerator.php
+++ Service/BarcodeGenerator.php
@@ -6,21 +6,47 @@
 
 namespace Stev\BarcodeBundle\Service;
 
+use Com\Tecnick\Barcode\Barcode;
+use Stev\BarcodeBundle\Model\Type;
+
 class BarcodeGenerator
 {
-    public function generateHtml(string $data)
+
+    /**
+     * @var Barcode
+     */
+    private $generator;
+
+    /**
+     * BarcodeGenerator constructor.
+     */
+    public function __construct()
     {
-        // instantiate the barcode class
-        $barcode = new \Com\Tecnick\Barcode\Barcode();
+        $this->generator = new \Com\Tecnick\Barcode\Barcode();
+    }
+
+    /**
+     * Get the low level barcode generator to use it as you wish
+     * @return Barcode
+     */
+    public function getGenerator(): Barcode
+    {
+        return $this->generator;
+    }
+
+    public function generateHtml(string $data, string $barcodeType = Type::QRCODE, array $params = []): string
+    {
+
+        $type = $barcodeType.','.implode(',', $params);
 
         // generate a barcode
-        $bobj = $barcode->getBarcodeObj(
-            'QRCODE,H',                     // barcode type and additional comma-separated parameters
+        $bobj = $this->generator->getBarcodeObj(
+            $type,                     // barcode type and additional comma-separated parameters
             $data,          // data string to encode
-            -4,                             // bar width (use absolute or negative value as multiplication factor)
-            -4,                             // bar height (use absolute or negative value as multiplication factor)
+            -8,                             // bar width (use absolute or negative value as multiplication factor)
+            -8,                             // bar height (use absolute or negative value as multiplication factor)
             'black',                        // foreground color
-            [-2, -2, -2, -2]           // padding (use absolute or negative values as multiplication factors)
+//            [-2, -2, -2, -2]           // padding (use absolute or negative values as multiplication factors)
         )->setBackgroundColor('white'); // background color
 
         // output the barcode as HTML div (see other output formats in the documentation and examples)
